6bf5a04529f16c086aef09ea79224dfc5c554a84
Adrian Tang <adrian.s.tang@gmail.com>
2025-07-01 18:55:37 -0400

    Configure for TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./revenue_intelligence/templates/**/*.html", "./revenue_intelligence/templates/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
