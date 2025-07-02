d7a5250c4655b92cd01dba3c3b047376430c0adf
Adrian Tang <adrian.s.tang@gmail.com>
2025-07-02 13:51:04 -0400

    Add analytics app

--- config/settings/base.py
+++ config/settings/base.py
@@ -86,6 +86,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "revenue_intelligence.users",
     # Your stuff: custom apps go here
+    "revenue_intelligence.analytics",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
